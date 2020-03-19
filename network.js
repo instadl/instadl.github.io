8a4452e46dc7e22d7f31b0861f36e2970721193d
Phương Mỹ Chi <phuongmychifc@24hgame.vn>
2020-03-19 17:39:39 +0700

    Update network.js

--- network.js
+++ network.js
@@ -3,14 +3,14 @@ window.addEventListener('load', function(e) {
         // $('#lovemychi').html("Online");
     updateConnectionStatus('Online', true);
   } else {
-        alert('Offline');
+        alert('Mất kết nối với internet, trang web cần kết nối để hoạt động dịch vụ');
     updateConnectionStatus('Offline', true);
   }
 }, false);
 
 window.addEventListener('online', function(e) {
   $(document).ready(function(){
-      $('#lovemychi').html("Online");
+      $('#lovemychi').html("Đã kết nối trở lại");
 
   })
 
@@ -21,9 +21,12 @@ window.addEventListener('online', function(e) {
 
 window.addEventListener('offline', function(e) {
   $(document).ready(function(){
+            alert('Mất kết nối với internet, trang web cần kết nối để hoạt động dịch vụ');
+
 $('#lovemychi').html("Mất kết nối với internet");
 
 });
   updateConnectionStatus('Offline', true);
+
   // Use offine mode.
 }, false);
