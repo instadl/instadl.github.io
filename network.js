34aec41ff3a8350ad8db0ac23808538481f015cb
Phương Mỹ Chi <phuongmychifc@24hgame.vn>
2020-03-20 12:44:07 +0700

    Update network.js

--- network.js
+++ network.js
@@ -4,7 +4,8 @@ window.addEventListener('load', function(e) {
     updateConnectionStatus('Online', true);
   } else {
         alert("Mất kết nối với internet");
-        $('#network').html("Đã Mất kết nối với internet,vui lòng kiểm tra lại");
+        $("body").html("<i class='material-icons' style='font-size:250px;margin-left:5%'>signal_wifi_off</i><br/>Mất kết nối với internet,Trang web cần có kết nối internet để sử dụng dịch vụ, xin vui lòng kiểm tra lại");
+        $('#network').html(" <i class='material-icons'>signal_wifi_off</i> Đã Mất kết nối với internet,vui lòng kiểm tra lại");
 
     updateConnectionStatus('Offline', true);
   }
@@ -25,7 +26,7 @@ window.addEventListener('offline', function(e) {
   $(document).ready(function(){
     alert("Mất kết nối với internet");
 
-$('#network').html("Đã Mất kết nối với internet,vui lòng kiểm tra lại");
+$('#network').html("<i class='material-icons'>signal_wifi_off</i> Đã Mất kết nối với internet,vui lòng kiểm tra lại");
 
 });
   updateConnectionStatus('Offline', true);
